fc3a055ede694d37d5f031dff9f6e3b4407075c9
Takuya Takeuchi <spitzbergen@gmail.com>
2017-11-12 14:40:03 +0900

    Fix namespace

--- examples/RandomCropper/Program.cs
+++ examples/RandomCropper/Program.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using DlibDotNet;
-using DlibDotNet.ImageProcessing;
 
 namespace RandomCropper
 {
--- src/DlibDotNet/ImageTransforms/RandomCropper.cs
+++ src/DlibDotNet/ImageTransforms/RandomCropper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using DlibDotNet.Extensions;
-using DlibDotNet.ImageProcessing;
 
 namespace DlibDotNet.ImageTransforms
 {
