efd579c50f1a3df4784fdd82feb7d241fe649021
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-09-15 15:52:43 +0900

    feat: add RectDetection

--- src/DlibDotNet.Native/dlib/image_processing/object_detector.h
+++ src/DlibDotNet.Native/dlib/image_processing/object_detector.h
@@ -8,6 +8,31 @@
 using namespace dlib;
 using namespace std;
 
+#pragma region rect_detection
+
+DLLEXPORT rectangle* rect_detection_get_rect(rect_detection* detection)
+{
+    const rectangle rect = detection->rect;
+    return new rectangle(rect);
+}
+
+DLLEXPORT double rect_detection_get_detection_confidence(rect_detection* detection)
+{
+    return detection->detection_confidence;
+}
+
+DLLEXPORT unsigned long rect_detection_get_weight_index(rect_detection* detection)
+{
+    return detection->weight_index;
+}
+
+DLLEXPORT void rect_detection_delete(rect_detection* obj)
+{
+	delete obj;
+}
+
+#pragma endregion rect_detection
+
 #pragma region full_detection
 
 DLLEXPORT full_object_detection* full_detection_get_rect(full_detection* detection)
