7825edc3479c4e9b85890170e3b0992fa6c7fc67
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-09-15 16:10:22 +0900

    feat: add new FrontalFaceDetector.Operator

--- src/DlibDotNet/ImageProcessing/FrontalFaceDetector.cs
+++ src/DlibDotNet/ImageProcessing/FrontalFaceDetector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using DlibDotNet.Extensions;
 
@@ -69,6 +70,33 @@ namespace DlibDotNet
             }
         }
 
+        public void Operator(MatrixBase image, out IEnumerable<RectDetection> detections, double threshold = 0d)
+        {
+            this.ThrowIfDisposed();
+
+            if (image == null)
+                throw new ArgumentNullException(nameof(image));
+
+            image.ThrowIfDisposed();
+
+            using (var dets = new StdVector<RectDetection>())
+            {
+                var inType = image.MatrixElementType.ToNativeMatrixElementType();
+                var ret = Native.frontal_face_detector_matrix_operator2(this.NativePtr,
+                                                                        inType, 
+                                                                        image.NativePtr,
+                                                                        threshold,
+                                                                        dets.NativePtr);
+                switch (ret)
+                {
+                    case Dlib.Native.ErrorType.InputElementTypeNotSupport:
+                        throw new ArgumentException($"Input {inType} is not supported.");
+                }
+
+                detections = dets.ToArray();
+            }
+        }
+
         #region Overrides
 
         protected override void DisposeUnmanaged()
@@ -104,6 +132,13 @@ namespace DlibDotNet
                                                                                              IntPtr img,
                                                                                              double adjustThreshold,
                                                                                              IntPtr dets);
+
+            [DllImport(NativeMethods.NativeLibrary, CallingConvention = NativeMethods.CallingConvention)]
+            public static extern Dlib.Native.ErrorType frontal_face_detector_matrix_operator2(IntPtr detector,
+                                                                                              Dlib.Native.MatrixElementType imgType,
+                                                                                              IntPtr img,
+                                                                                              double adjustThreshold,
+                                                                                              IntPtr dets);
 
             [DllImport(NativeMethods.NativeLibrary, CallingConvention = NativeMethods.CallingConvention)]
             public static extern void frontal_face_detector_delete(IntPtr detector);
