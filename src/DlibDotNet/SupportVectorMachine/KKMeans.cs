72526f6236063f752d656bfc7abcc3be45388d5d
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-09-17 18:30:54 +0900

    feat: add Dlib.FindClustersUsingAngularKMeans

--- src/DlibDotNet/SupportVectorMachine/KKMeans.cs
+++ src/DlibDotNet/SupportVectorMachine/KKMeans.cs
@@ -13,6 +13,53 @@ namespace DlibDotNet
 
         #region Methods
 
+        public static void FindClustersUsingAngularKMeans<T>(IEnumerable<Matrix<T>> centers, IEnumerable<Matrix<T>> samples, uint maxIteration = 1000)
+            where T : struct
+        {
+            if (centers == null)
+                throw new ArgumentNullException(nameof(centers));
+            if (samples == null)
+                throw new ArgumentNullException(nameof(samples));
+
+            var centerArray = centers.ToArray();
+            if (!centerArray.Any())
+                return;
+
+            var sampleArray = samples.ToArray();
+            if (!sampleArray.Any())
+                return;
+
+            var center = centerArray.FirstOrDefault();
+            if (center == null)
+                throw new ArgumentException($"{nameof(centers)} contains null object", nameof(samples));
+
+            var sample = sampleArray.FirstOrDefault();
+            if (sample == null)
+                throw new ArgumentException($"{nameof(samples)} contains null object", nameof(samples));
+
+            var templateRow = sample.TemplateRows;
+            var templateColumn = sample.TemplateColumns;
+
+            using (var inSamples = new StdVector<Matrix<T>>(sampleArray, templateRow, templateColumn))
+            using (var inCenters = new StdVector<Matrix<T>>(centerArray, templateRow, templateColumn))
+            {
+                var type = sample.MatrixElementType.ToNativeMatrixElementType();
+                var ret = Native.find_clusters_using_angular_kmeans(type,
+                                                                    templateRow,
+                                                                    templateColumn,
+                                                                    inSamples.NativePtr,
+                                                                    inCenters.NativePtr,
+                                                                    maxIteration);
+                switch (ret)
+                {
+                    case Native.ErrorType.MatrixElementTypeNotSupport:
+                        throw new ArgumentException($"{type} is not supported.");
+                    case Native.ErrorType.MatrixElementTemplateSizeNotSupport:
+                        throw new ArgumentException($"{nameof(sample.TemplateColumns)} or {nameof(sample.TemplateRows)} is not supported.");
+                }
+            }
+        }
+
         public static IEnumerable<Matrix<T>> PickInitialCenters<T>(int numberCenters, IEnumerable<Matrix<T>> samples, LinearKernel<Matrix<T>> k, double percentile = 0.01)
             where T : struct
         {
@@ -67,6 +114,14 @@ namespace DlibDotNet
 
         internal sealed partial class Native
         {
+
+            [DllImport(NativeMethods.NativeLibrary, CallingConvention = NativeMethods.CallingConvention)]
+            public static extern ErrorType find_clusters_using_angular_kmeans(MatrixElementType type,
+                                                                              int templateRows,
+                                                                              int templateColumns,
+                                                                              IntPtr centers,
+                                                                              IntPtr samples,
+                                                                              uint max_iter);
 
             [DllImport(NativeMethods.NativeLibrary, CallingConvention = NativeMethods.CallingConvention)]
             public static extern ErrorType pick_initial_centers(MatrixElementType elementType,
