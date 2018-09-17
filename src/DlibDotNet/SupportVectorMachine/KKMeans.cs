338090ddd36095451bf6fa27d735a7bb9e46f482
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-09-17 16:08:56 +0900

    feat: add Dlib.PickInitialCenters

---
+++ src/DlibDotNet/SupportVectorMachine/KKMeans.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using DlibDotNet.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace DlibDotNet
+{
+
+    public static partial class Dlib
+    {
+
+        #region Methods
+
+        public static IEnumerable<Matrix<T>> PickInitialCenters<T>(int numberCenters, IEnumerable<Matrix<T>> samples, LinearKernel<Matrix<T>> k, double percentile = 0.01)
+            where T : struct
+        {
+            if (samples == null)
+                throw new ArgumentNullException(nameof(samples));
+
+            var sampleArray = samples.ToArray();
+            if (!sampleArray.Any())
+                return new Matrix<T>[0];
+
+            var first = sampleArray.FirstOrDefault();
+            if (first == null)
+                throw new ArgumentException($"{nameof(samples)} contains null object", nameof(samples));
+
+            var templateRow = first.TemplateRows;
+            var templateColumn = first.TemplateColumns;
+            foreach (var sample in sampleArray)
+            {
+                if (sample == null)
+                    throw new ArgumentException($"{nameof(samples)} contains null object", nameof(samples));
+                if (sample.TemplateRows != templateRow)
+                    throw new ArgumentException($"{nameof(samples)} contains different {nameof(sample.TemplateRows)} of {typeof(Matrix<T>).Name}", nameof(samples));
+                if (sample.TemplateColumns != templateColumn)
+                    throw new ArgumentException($"{nameof(samples)} contains different {nameof(sample.TemplateColumns)} of {typeof(Matrix<T>).Name}", nameof(samples));
+            }
+
+            using (var inSamples = new StdVector<Matrix<T>>(sampleArray, templateRow, templateColumn))
+            using (var outCenters = new StdVector<Matrix<T>>(templateRow, templateColumn))
+            {
+                var type = first.MatrixElementType.ToNativeMatrixElementType();
+                var ret = Native.pick_initial_centers(type,
+                                                      templateRow,
+                                                      templateColumn,
+                                                      numberCenters,
+                                                      outCenters.NativePtr,
+                                                      inSamples.NativePtr,
+                                                      k.NativePtr,
+                                                      percentile);
+                switch (ret)
+                {
+                    case Native.ErrorType.MatrixElementTypeNotSupport:
+                        throw new ArgumentException($"{type} is not supported.");
+                    case Native.ErrorType.MatrixElementTemplateSizeNotSupport:
+                        throw new ArgumentException($"{nameof(first.TemplateColumns)} or {nameof(first.TemplateRows)} is not supported.");
+                }
+
+                return outCenters.ToArray();
+            }
+        }
+
+        #endregion
+
+        internal sealed partial class Native
+        {
+
+            [DllImport(NativeMethods.NativeLibrary, CallingConvention = NativeMethods.CallingConvention)]
+            public static extern ErrorType pick_initial_centers(MatrixElementType elementType,
+                                                                int templateRows,
+                                                                int templateColumns,
+                                                                long num_centers,
+                                                                IntPtr centers,
+                                                                IntPtr samples,
+                                                                IntPtr k,
+                                                                double percentile);
+
+        }
+
+    }
+
+}
