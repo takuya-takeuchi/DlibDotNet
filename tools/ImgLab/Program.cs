7acc8957ea134e446df4f1cfe2d5aea3c86da9ae
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-09-23 16:38:13 +0900

    feat: add CreateNewDataset

--- tools/ImgLab/Program.cs
+++ tools/ImgLab/Program.cs
@@ -2,6 +2,12 @@
  * This sample program is ported by C# from tools\imglab.
 */
 
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DlibDotNet;
+using DlibDotNet.ImageDatasetMetadata;
 using Microsoft.Extensions.CommandLineUtils;
 
 namespace ImgLab
@@ -26,13 +32,38 @@ namespace ImgLab
             app.Name = nameof(ImgLab);
             app.HelpOption("-h|--help");
 
-            var createOption = app.Option("-c|--create", "", CommandOptionType.SingleValue);
-            var clusterOption = app.Option("-cluster|--cluster", "", CommandOptionType.SingleValue);
+            var createOption = new CommandOption("-c|--create", CommandOptionType.SingleValue);
+            var convertOption = new CommandOption("-convert|--convert", CommandOptionType.NoValue);
+            var clusterOption = new CommandOption("-cluster|--cluster", CommandOptionType.SingleValue);
+
+            app.Options.Add(createOption);
+            app.Options.Add(clusterOption);
+            app.Options.Add(new CommandOption("-r|--r", CommandOptionType.NoValue));
+            app.Options.Add(convertOption);
 
             app.OnExecute(() =>
             {
                 if (createOption.HasValue())
                 {
+                    if (convertOption.HasValue())
+                    {
+                        var value = convertOption.Value();
+                        switch (value)
+                        {
+                            case "pascal-xml":
+                                break;
+                            case "pascal-v1":
+                                break;
+                            case "idl":
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        CreateNewDataset(app);
+                    }
+
+                    return ExitSuccess;
                 }
 
                 if (clusterOption.HasValue())
@@ -43,6 +74,83 @@ namespace ImgLab
 
             app.Execute(args);
         }
+
+        #region Helpers
+
+        private static void CreateNewDataset(CommandLineApplication parser)
+        {
+            var createOption = parser.GetOptions().FirstOrDefault(option => option.ShortName == "c");
+            var depthOption = parser.GetOptions().FirstOrDefault(option => option.ShortName == "r");
+
+            var filename = createOption.Value();
+
+            // make sure the file exists so we can use the get_parent_directory() command to
+            // figure out it's parent directory.
+            MakeEmptyFile(filename);
+            var parentDir = Path.GetDirectoryName(Path.GetFullPath(filename));
+
+            var depth = 0;
+            if (depthOption.HasValue())
+                depth = 30;
+
+            using (var meta = new Dataset())
+            {
+                meta.Name = "imglab dataset";
+                meta.Comment = "Created by imglab tool.";
+
+                var images = new List<Image>();
+                for (var i = 0; i < parser.RemainingArguments.Count; ++i)
+                {
+                    var arg = parser.RemainingArguments[i];
+
+                    try
+                    {
+                        if (!File.Exists(arg))
+                            throw new FileNotFoundException();
+
+                        var temp = StripPath(arg, parentDir);
+                        images.Add(new Image(temp));
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        // then parser[i] should be a directory
+                        const string ext = "(.png|.PNG|.jpeg|.JPEG|.jpg|.JPG|.bmp|.BMP|.dng|.DNG|.gif|.GIF)$";
+                        var files = GetFilesMostDeep(arg, ext, depth).ToList();
+                        files.Sort();
+
+                        foreach (var t in files)
+                            images.Add(new Image(StripPath(t, parentDir)));
+                    }
+
+                    meta.Images = images.ToArray();
+
+                    Dlib.ImageDatasetMetadata.SaveImageDatasetMetadata(meta, filename);
+                }
+            }
+        }
+
+        public static string[] GetFilesMostDeep(string rootPath, string pattern, int depth)
+        {
+            var list = Directory.GetFiles(rootPath)
+                                .Where(s => Regex.IsMatch(s, pattern, RegexOptions.Compiled))
+                                .Select(Path.GetFullPath).ToList();
+            if (depth > 0)
+            {
+                foreach (var stDirPath in Directory.GetDirectories(rootPath))
+                {
+                    var paths = GetFilesMostDeep(stDirPath, pattern, depth - 1);
+                    if (paths != null)
+                        list.AddRange(paths);
+                }
+            }
+
+            var returns = new string[list.Count];
+            list.CopyTo(returns, 0);
+
+            return returns;
+        }
+
+        #endregion
 
         #endregion
 
