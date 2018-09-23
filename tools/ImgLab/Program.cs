0f2374769c1aa567bf40a7361b020711128be149
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-09-23 12:12:20 +0900

    feat: add ClusterDataset

---
+++ tools/ImgLab/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * This sample program is ported by C# from tools\imglab.
+*/
+
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace ImgLab
+{
+
+    partial class Program
+    {
+
+        #region Fields
+
+        private const int ExitSuccess = 0;
+
+        private const int ExitFailure = 1;
+
+        #endregion
+
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            var app = new CommandLineApplication(false);
+            app.Name = nameof(ImgLab);
+            app.HelpOption("-h|--help");
+
+            var createOption = app.Option("-c|--create", "", CommandOptionType.SingleValue);
+            var clusterOption = app.Option("-cluster|--cluster", "", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                if (createOption.HasValue())
+                {
+                }
+
+                if (clusterOption.HasValue())
+                    ClusterDataset(app);
+
+                return 0;
+            });
+
+            app.Execute(args);
+        }
+
+        #endregion
+
+    }
+
+}
