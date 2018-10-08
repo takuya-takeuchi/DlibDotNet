ea77a9860ec45d3c17c82b654eef1d81127da780
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-10-08 14:32:13 +0900

    chore: update version

--- nuget/BuildNuspec.bat
+++ nuget/BuildNuspec.bat
@@ -1,2 +1,12 @@
-nuget pack DlibDotNet-CPU.nuspec 
+dotnet build -c Release ..\src\DlibDotNet
+
+nuget pack DlibDotNet-CPU.nuspec
 nuget pack DlibDotNet-CUDA.nuspec
+
+mkdir Release
+mkdir Release\DlibDotNet-CPU
+xcopy ..\src\DlibDotNet.Native\build_cpu\Release\DlibDotNet.Native.dll Release\DlibDotNet-CPU /y
+xcopy ..\src\DlibDotNet.Native.Dnn\build_cpu\Release\DlibDotNet.Native.Dnn.dll Release\DlibDotNet-CPU /y
+mkdir Release\DlibDotNet-CUDA
+xcopy ..\src\DlibDotNet.Native\build_cuda\Release\DlibDotNet.Native.dll Release\DlibDotNet-CUDA /y
+xcopy ..\src\DlibDotNet.Native.Dnn\build_cuda\Release\DlibDotNet.Native.Dnn.dll Release\DlibDotNet-CUDA /y
