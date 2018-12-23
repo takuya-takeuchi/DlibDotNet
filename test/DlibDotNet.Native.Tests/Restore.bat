226eb66c9665ef4ad101f659efc361a1df6c7785
Valery Schepaschenko <valery.sntx@gmail.com>
2018-12-24 01:15:29 +0200

    [fix] - fixing .targets evaluation for DlibDotNet

--- test/DlibDotNet.Native.Tests/Restore.bat
+++ test/DlibDotNet.Native.Tests/Restore.bat
@@ -1,5 +1,7 @@
-
+rd %USERPROFILE%\.nuget\packages\dlibdotnet\ /Q /S
+rd %USERPROFILE%\.nuget\packages\dlibdotnet.native\ /Q /S
 nuget config -set repositoryPath=%USERPROFILE%\.nuget -configfile nuget.config
 dotnet add PackageReference.csproj package "DlibDotNet" -f net472 -s %USERPROFILE%\.nuget\
+rem dotnet add PackageReference.csproj package "DlibDotNet" -s https://www.myget.org/F/nuget-dotnet/api/v3/index.json
 dotnet restore --force-evaluate -v detailed --configfile nuget.config
 dotnet build -c Release
