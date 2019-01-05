7bae3f4ce797df0fe1a97f5326d0a9512527a4f3
Your Name <you@example.com>
2019-01-05 07:12:44 +0200

    wip - packaging linux-x64 native runtime deps

--- src/DlibDotNet/NativeMethods.cs
+++ src/DlibDotNet/NativeMethods.cs
@@ -12,13 +12,17 @@ namespace DlibDotNet
         public const string NativeDnnLibrary = "libDlibDotNet.Native.Dnn.so";
 
         public const CallingConvention CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl;
-#elif MAC
+#endif
+
+#if MAC
         public const string NativeLibrary = "libDlibDotNet.Native.dylib";
-    
         public const string NativeDnnLibrary = "libDlibDotNet.Native.Dnn.dylib";
 
         public const CallingConvention CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl;
-#else
+#endif
+
+
+#if WINDOWS
         public const string NativeLibrary = "DlibDotNet.Native.dll";
 
         public const string NativeDnnLibrary = "DlibDotNet.Native.Dnn.dll";
