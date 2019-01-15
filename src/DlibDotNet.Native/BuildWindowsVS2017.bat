76ae14b20738d8adb218f01c174f47e07a5bbac2
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-01-15 17:54:35 +0900

    feat :add build script for Windows

---
+++ src/DlibDotNet.Native/BuildWindowsVS2017.bat
@@ -0,0 +1,37 @@
+@echo off
+@rem ***************************************
+@rem Arguments
+@rem %1: Build Configuration (Release/Debug)
+@rem ***************************************
+
+if "%1"=="" ( 
+@echo Error: Speficy build configuration [Release/Debug]
+@exit /B
+)
+
+if "%2"=="" ( 
+@echo Error: Speficy Target [cpu/cuda]
+@exit /B
+)
+
+if "%2"=="cpu" (
+  set CURDIR=%cd%
+  set OUTPUT=build_%2
+  mkdir %OUTPUT% && cd %OUTPUT%
+  cmake -G "Visual Studio 15 2017 Win64" -T host=x64 ^
+        -D DLIB_USE_CUDA=OFF ^
+        ..
+) else if "%2"=="cuda"  (
+  set CURDIR=%cd%
+  set OUTPUT=build_%2
+  mkdir %OUTPUT% && cd %OUTPUT%
+  cmake -G "Visual Studio 15 2017 Win64" -T host=x64 ^
+        -D DLIB_USE_CUDA=ON ^
+        ..
+) else (
+  @echo Error: Target should be [cpu/cuda]
+  @exit /B
+)
+
+cmake --build . --config %1
+cd %CURDIR%
