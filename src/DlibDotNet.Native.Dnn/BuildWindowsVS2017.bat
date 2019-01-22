090cd9d03c3438838e86c0d47d6c051c2e453f5e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-01-22 23:49:48 +0900

    fix: use enabledelayedexpansion

--- src/DlibDotNet.Native.Dnn/BuildWindowsVS2017.bat
+++ src/DlibDotNet.Native.Dnn/BuildWindowsVS2017.bat
@@ -1,40 +1,39 @@
+@setlocal enabledelayedexpansion
 @echo off
 @rem ***************************************
 @rem Arguments
 @rem %1: Build Configuration (Release/Debug)
+@rem %: Target (cpu/cuda)
 @rem ***************************************
 
 if "%1"=="" ( 
-@echo Error: Speficy build configuration [Release/Debug]
-@exit /B
+  @echo Error: Speficy build configuration [Release/Debug]
+  @exit /B
 )
 
 if "%2"=="" ( 
-@echo Error: Speficy Target [cpu/cuda]
-@exit /B
+  @echo Error: Speficy Target [cpu/cuda]
+  @exit /B
 )
 
+set CURDIR=%cd%
+set OUTPUT=build_%2
+
 if "%2"=="cpu" (
-  set CURDIR=%cd%
   set OUTPUT=build_%2
-  if not exist %OUTPUT% (
-     mkdir %OUTPUT%
-  )
+  if not exist %OUTPUT% mkdir %OUTPUT%
   cd %OUTPUT%
   cmake -G "Visual Studio 15 2017 Win64" -T host=x64 ^
         -D DLIB_USE_CUDA=OFF ^
         ..
-) else if "%2"=="cuda"  (
-  set CURDIR=%cd%
+) else if "%2"=="cuda" (
   set OUTPUT=build_%2
-  if not exist %OUTPUT% (
-     mkdir %OUTPUT%
-  )
+  if not exist %OUTPUT% mkdir %OUTPUT%
   cd %OUTPUT%
   cmake -G "Visual Studio 15 2017 Win64" -T host=x64 ^
         -D DLIB_USE_CUDA=ON ^
         ..
-) else (
+) else ( 
   @echo Error: Target should be [cpu/cuda]
   @exit /B
 )
