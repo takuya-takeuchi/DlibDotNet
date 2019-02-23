ab0887a7df2d26b599e5b3155b7c28e7cf87b2e8
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-02-23 21:44:52 +0900

    chore: build script for osx

---
+++ nuget/BuildOSX.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ROOT=`pwd`
+
+# Get root of DlibDotNet directory
+DDNDIR=`dirname $(pwd)`
+
+CONFIG=Release
+NATIVE=${DDNDIR}/src/DlibDotNet.Native
+NATIVEDNN=${DDNDIR}/src/DlibDotNet.Native.Dnn
+
+arch=(
+    "cpu"
+    #"cuda"
+)
+
+for a in "${arch[@]}" ; do
+    cd ${NATIVE}
+    ./BuildUnix.sh ${CONFIG} ${a}
+    cp ${NATIVE}/build_osx_${a}/libDlibDotNetNative.dylib ${ROOT}/${a}/runtimes/osx-x64/native
+done
+
+for a in "${arch[@]}" ; do
+    cd ${NATIVEDNN}
+    ./BuildUnix.sh ${CONFIG} ${a}
+    cp ${NATIVEDNN}/build_osx_${a}/libDlibDotNetNativeDnn.dylib ${ROOT}/${a}/runtimes/osx-x64/native
+done
+
+cd ${ROOT}
