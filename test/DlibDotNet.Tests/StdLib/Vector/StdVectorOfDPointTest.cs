4183561220c4b1edf21b5232034307aef10d1ba5
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-03-31 04:17:33 +0900

    chore: add tests

---
+++ test/DlibDotNet.Tests/StdLib/Vector/StdVectorOfDPointTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using DlibDotNet.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable once CheckNamespace
+namespace DlibDotNet
+{
+
+    [TestClass]
+    public class StdVectorOfPointTest : TestBase
+    {
+
+        [TestMethod]
+        public void Create()
+        {
+            var vector = new StdVector<Point>();
+            this.DisposeAndCheckDisposedState(vector);
+        }
+
+        [TestMethod]
+        public void CreateWithSize()
+        {
+            const int size = 10;
+            var vector = new StdVector<Point>(size);
+            this.DisposeAndCheckDisposedState(vector);
+        }
+
+        [TestMethod]
+        public void CreateWithCollection()
+        {
+            const int size = 10;
+            var source = Enumerable.Range(0, size).Select(i => new Point(i, i));
+            var vector = new StdVector<Point>(source);
+            Assert.AreEqual(vector.Size, size);
+            var ret = vector.ToArray();
+            for (var i = 0; i < size; i++)
+            {
+                Assert.AreEqual(ret[i].X, i);
+                Assert.AreEqual(ret[i].Y, i);
+            }
+            this.DisposeAndCheckDisposedState(vector);
+        }
+
+    }
+
+}
---
+++ test/DlibDotNet.Tests/StdLib/Vector/StdVectorOfStdString.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using DlibDotNet.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable once CheckNamespace
+namespace DlibDotNet
+{
+
+    [TestClass]
+    public class StdVectorOfStdString : TestBase
+    {
+
+        [TestMethod]
+        public void Create()
+        {
+            var vector = new StdVector<StdString>();
+            this.DisposeAndCheckDisposedState(vector);
+        }
+
+        [TestMethod]
+        public void CreateWithSize()
+        {
+            const int size = 10;
+            var vector = new StdVector<StdString>(size);
+            this.DisposeAndCheckDisposedState(vector);
+        }
+
+        [TestMethod]
+        public void CreateWithCollection()
+        {
+            const int size = 10;
+            var source = Enumerable.Range(0, size).Select(i => new StdString(i.ToString()));
+            var vector = new StdVector<StdString>(source);
+            Assert.AreEqual(vector.Size, size);
+            var ret = vector.ToArray();
+            for (var i = 0; i < size; i++)
+            {
+                Assert.AreEqual(ret[i].ToString(), i.ToString());
+            }
+            this.DisposeAndCheckDisposedState(vector);
+        }
+
+    }
+
+}
