329bd594a821e39a73cca6a62f7065b4239e57bb
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-04-30 05:16:59 +0900

    fix: alpha value was dropped

--- examples/DnnMmodFindCars/Program.cs
+++ examples/DnnMmodFindCars/Program.cs
@@ -145,7 +145,7 @@ namespace DnnMmodFindCars
                                                 Dlib.AssignPixel(ref p, Dlib.ColormapJet(val, lower, upper));
                                                 p.Alpha = 120;
 
-                                                var rgb = new RgbPixel();
+                                                var rgb = tiledImg[r, c];
                                                 Dlib.AssignPixel(ref rgb, p);
                                                 tiledImg[r, c] = rgb;
                                             }
@@ -203,7 +203,7 @@ namespace DnnMmodFindCars
                                                     Dlib.AssignPixel(ref p, Dlib.ColormapJet(maxScore, lower, upper));
                                                     p.Alpha = 120;
 
-                                                    var rgb = new RgbPixel();
+                                                    var rgb = img[r, c];
                                                     Dlib.AssignPixel(ref rgb, p);
                                                     img[r, c] = rgb;
                                                 }
