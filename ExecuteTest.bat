7ee88fdb14190296b4717d567e600b754096c0be
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-04-30 01:02:47 +0900

    chore: add/update test script

--- ExecuteTest.bat
+++ ExecuteTest.bat
@@ -3,6 +3,7 @@
 @rem ***************************************
 @rem Arguments
 @rem %1: Copy TestData (1:true, 0:false)
+@rem %2: Target (cpu/cuda/arm)
 @rem ***************************************
 
 if "%1"=="" ( 
@@ -10,12 +11,27 @@ if "%1"=="" (
   @exit /B
 )
 
+if "%2"=="" ( 
+  @echo Error: Target [cpu/cuda/arm]
+  @exit /B
+)
+
+@set OUTPUT=build_win_%2_x64
+
+@set TARGET=test\DlibDotNet.Tests\bin\Release\netcoreapp2.1
+
 if "%1"=="1" (
-  set DST_DATA=test\DlibDotNet.Tests\bin\Release\netcoreapp2.1\data
+  @set DST_DATA=!TARGET!\data
   if NOT EXIST "!DST_DATA!" (
      mkdir "!DST_DATA!"
   )
-  xcopy test\DlibDotNet.Tests\data "!DST_DATA!" /y /s
+  @xcopy test\DlibDotNet.Tests\data "!DST_DATA!" /y /s
 )
+
+@set NATIVEDIR=src\DlibDotNet.Native\!OUTPUT!
+@set NATIVEDNNDIR=src\DlibDotNet.Native.Dnn\!OUTPUT!
+
+@copy !NATIVEDIR!\Release\DlibDotNetNative.dll !TARGET! /y
+@copy !NATIVEDNNDIR!\Release\DlibDotNetNativeDnn.dll !TARGET! /y
 
 dotnet test test\DlibDotNet.Tests\DlibDotNet.Tests.csproj -c Release
