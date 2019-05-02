b74e183cafb7792899b1b54162ee04891cd1a16e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-05-02 15:18:37 +0900

    fix: simplified and remove 'delete void*'

--- src/DlibDotNet.Native/dlib/matrix/matrix_expressions.h
+++ src/DlibDotNet.Native/dlib/matrix/matrix_expressions.h
@@ -4,201 +4,88 @@
 #include "../export.h"
 #include <dlib/matrix.h>
 #include <dlib/matrix/matrix_expressions.h>
+#include "../template.h"
 #include "../shared.h"
 
 using namespace dlib;
 using namespace std;
 
-DLLEXPORT void matrix_range_exp_delete(void* obj)
+#pragma region template
+
+#define MAKE_FUNC(__TYPE__, __TYPENAME__)\
+DLLEXPORT matrix_range_exp<__TYPE__>* matrix_range_exp_create_##__TYPENAME__##_new1(__TYPE__ start, __TYPE__ inc, __TYPE__ end)\
+{\
+    return new matrix_range_exp<__TYPE__>(start, inc, end);\
+}\
+\
+DLLEXPORT matrix_range_exp<__TYPE__>* matrix_range_exp_create_##__TYPENAME__##_new2(__TYPE__ start, __TYPE__ end, long num)\
+{\
+    /* true is dummy */ \
+    return new matrix_range_exp<__TYPE__>(start, end, num, true);\
+}\
+\
+DLLEXPORT matrix_range_exp<__TYPE__>* matrix_range_exp_create_##__TYPENAME__##_new3(__TYPE__ start, __TYPE__ end)\
+{\
+    return new matrix_range_exp<__TYPE__>(start, end);\
+}\
+
+#define matrix_range_exp_delete_template(__TYPE__, error, __ELEMENT_TYPE__, ...) \
+delete ((dlib::matrix_range_exp<__TYPE__>*)matrix);
+
+#define matrix_range_exp_nc_template(__TYPE__, error, __ELEMENT_TYPE__, ...) \
+*result = ((dlib::matrix_range_exp<__TYPE__>*)matrix)->nc();
+
+#define matrix_range_exp_nr_template(__TYPE__, error, __ELEMENT_TYPE__, ...) \
+*result = ((dlib::matrix_range_exp<__TYPE__>*)matrix)->nr();
+
+#pragma endregion template
+
+DLLEXPORT void matrix_range_exp_delete(matrix_element_type type, void* matrix)
 {
-	delete obj;
+    int error = ERR_OK;
+
+    matrix_nosize_template(type,
+                           error,
+                           matrix_range_exp_delete_template,
+                           matrix);
 }
 
-DLLEXPORT void* matrix_range_exp_create(matrix_element_type type, matrix_range_exp_create_param* param)
-{
-    void* result = nullptr;
-    switch(type)
-    {
-        case matrix_element_type::UInt8:
-            if (param->use_uint8_t_inc)
-                result = new matrix_range_exp<uint8_t>(param->uint8_t_start, param->uint8_t_inc, param->uint8_t_end);
-            else if (param->use_num)
-                result = new matrix_range_exp<uint8_t>(param->uint8_t_start, param->uint8_t_end, param->num);
-            else
-                result = new matrix_range_exp<uint8_t>(param->uint8_t_start, param->uint8_t_end);
-            break;
-        case matrix_element_type::UInt16:
-            if (param->use_uint16_t_inc)
-                result = new matrix_range_exp<uint16_t>(param->uint16_t_start, param->uint16_t_inc, param->uint16_t_end);
-            else if (param->use_num)
-                result = new matrix_range_exp<uint16_t>(param->uint16_t_start, param->uint16_t_end, param->num);
-            else
-                result = new matrix_range_exp<uint16_t>(param->uint16_t_start, param->uint16_t_end);
-            break;
-        case matrix_element_type::Int8:
-            if (param->use_int8_t_inc)
-                result = new matrix_range_exp<int8_t>(param->int8_t_start, param->int8_t_inc, param->int8_t_end);
-            else if (param->use_num)
-                result = new matrix_range_exp<int8_t>(param->int8_t_start, param->int8_t_end, param->num);
-            else
-                result = new matrix_range_exp<int8_t>(param->int8_t_start, param->int8_t_end);
-            break;
-        case matrix_element_type::Int16:
-            if (param->use_int16_t_inc)
-                result = new matrix_range_exp<int16_t>(param->int16_t_start, param->int16_t_inc, param->int16_t_end);
-            else if (param->use_num)
-                result = new matrix_range_exp<int16_t>(param->int16_t_start, param->int16_t_end, param->num);
-            else
-                result = new matrix_range_exp<int16_t>(param->int16_t_start, param->int16_t_end);
-            break;
-        case matrix_element_type::Int32:
-            if (param->use_int32_t_inc)
-                result = new matrix_range_exp<int32_t>(param->int32_t_start, param->int32_t_inc, param->int32_t_end);
-            else if (param->use_num)
-                result = new matrix_range_exp<int32_t>(param->int32_t_start, param->int32_t_end, param->num);
-            else
-                result = new matrix_range_exp<int32_t>(param->int32_t_start, param->int32_t_end);
-            break;
-        case matrix_element_type::Float:
-            if (param->use_float_inc)
-                result = new matrix_range_exp<float>(param->float_start, param->float_inc, param->float_end);
-            else if (param->use_num)
-                result = new matrix_range_exp<float>(param->float_start, param->float_end, param->num);
-            else
-                result = new matrix_range_exp<float>(param->float_start, param->float_end);
-            break;
-        case matrix_element_type::Double:
-            if (param->use_double_inc)
-                result = new matrix_range_exp<double>(param->double_start, param->double_inc, param->double_end);
-            else if (param->use_num)
-                result = new matrix_range_exp<double>(param->double_start, param->double_end, param->num);
-            else
-                result = new matrix_range_exp<double>(param->double_start, param->double_end);
-            break;
-        case matrix_element_type::UInt32:
-        case matrix_element_type::RgbPixel:
-        case matrix_element_type::HsiPixel:
-        case matrix_element_type::RgbAlphaPixel:
-        default:
-            result = nullptr;
-            break;
-    }
-
-    return result;
-}
+MAKE_FUNC(int8_t, int8_t)
+MAKE_FUNC(int16_t, int16_t)
+MAKE_FUNC(int32_t, int32_t)
+MAKE_FUNC(int64_t, int64_t)
+MAKE_FUNC(uint8_t, uint8_t)
+MAKE_FUNC(uint16_t, uint16_t)
+MAKE_FUNC(float, float)
+MAKE_FUNC(double, double)
+// not support due to std::abs has no proper overloads
+// MAKE_FUNC(uint32_t, uint32_t)
+// MAKE_FUNC(uint64_t, uint64_t)
 
 DLLEXPORT bool matrix_range_exp_nc(matrix_element_type type, void* matrix, int* result)
 {
-    bool err = false;
-    switch(type)
-    {
-        case matrix_element_type::UInt8:
-			*result = ((matrix_range_exp<uint8_t>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::UInt16:
-			*result = ((matrix_range_exp<uint16_t>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::UInt32:
-			*result = ((matrix_range_exp<uint32_t>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::Int8:
-			*result = ((matrix_range_exp<int8_t>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::Int16:
-			*result = ((matrix_range_exp<int16_t>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::Int32:
-			*result = ((matrix_range_exp<int32_t>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::Float:
-			*result = ((matrix_range_exp<float>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::Double:
-			*result = ((matrix_range_exp<double>*)matrix)->nc();
-			err = true;
-            break;
-        case matrix_element_type::RgbPixel:
-            *result = ((matrix_range_exp<rgb_pixel>*)matrix)->nc();
-            err = true;
-            break;
-        case matrix_element_type::HsiPixel:
-            *result = ((matrix_range_exp<hsi_pixel>*)matrix)->nc();
-            err = true;
-            break;
-        case matrix_element_type::RgbAlphaPixel:
-            *result = ((matrix_range_exp<rgb_alpha_pixel>*)matrix)->nc();
-            err = true;
-            break;
-        default:
-            err = false;
-            break;
-    }
+    int error = ERR_OK;
 
-    return err;
+    matrix_nosize_template(type,
+                           error,
+                           matrix_range_exp_nc_template,
+                           matrix,
+                           result);
+
+    return error == ERR_OK;
 }
 
 DLLEXPORT bool matrix_range_exp_nr(matrix_element_type type, void* matrix, int* result)
 {
-    bool err = false;
-    switch(type)
-    {
-        case matrix_element_type::UInt8:
-			*result = ((matrix_range_exp<uint8_t>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::UInt16:
-			*result = ((matrix_range_exp<uint16_t>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::UInt32:
-			*result = ((matrix_range_exp<uint32_t>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::Int8:
-			*result = ((matrix_range_exp<int8_t>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::Int16:
-			*result = ((matrix_range_exp<int16_t>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::Int32:
-			*result = ((matrix_range_exp<int32_t>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::Float:
-			*result = ((matrix_range_exp<float>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::Double:
-			*result = ((matrix_range_exp<double>*)matrix)->nr();
-			err = true;
-            break;
-        case matrix_element_type::RgbPixel:
-            *result = ((matrix_range_exp<rgb_pixel>*)matrix)->nr();
-            err = true;
-            break;
-        case matrix_element_type::HsiPixel:
-            *result = ((matrix_range_exp<hsi_pixel>*)matrix)->nr();
-            err = true;
-            break;
-        case matrix_element_type::RgbAlphaPixel:
-            *result = ((matrix_range_exp<rgb_alpha_pixel>*)matrix)->nr();
-            err = true;
-            break;
-        default:
-            err = false;
-            break;
-    }
+    int error = ERR_OK;
 
-    return err;
+    matrix_nosize_template(type,
+                           error,
+                           matrix_range_exp_nr_template,
+                           matrix,
+                           result);
+
+    return error == ERR_OK;
 }
 
 #endif
--- test/DlibDotNet.Tests/Matrix/MatrixRangeExpTest.cs
+++ test/DlibDotNet.Tests/Matrix/MatrixRangeExpTest.cs
@@ -17,12 +17,14 @@ namespace DlibDotNet.Tests.Matrix
                 new { Type = MatrixElementTypes.RgbPixel,      ExpectResult = false},
                 new { Type = MatrixElementTypes.RgbAlphaPixel, ExpectResult = false},
                 new { Type = MatrixElementTypes.HsiPixel,      ExpectResult = false},
+                new { Type = MatrixElementTypes.UInt64,         ExpectResult = false},
                 new { Type = MatrixElementTypes.UInt32,        ExpectResult = false},
                 new { Type = MatrixElementTypes.UInt8,         ExpectResult = true},
                 new { Type = MatrixElementTypes.UInt16,        ExpectResult = true},
                 new { Type = MatrixElementTypes.Int8,          ExpectResult = true},
                 new { Type = MatrixElementTypes.Int16,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Int32,         ExpectResult = true},
+                new { Type = MatrixElementTypes.Int64,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Float,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Double,        ExpectResult = true}
             };
@@ -64,12 +66,14 @@ namespace DlibDotNet.Tests.Matrix
                 new { Type = MatrixElementTypes.RgbPixel,      ExpectResult = false},
                 new { Type = MatrixElementTypes.RgbAlphaPixel, ExpectResult = false},
                 new { Type = MatrixElementTypes.HsiPixel,      ExpectResult = false},
+                new { Type = MatrixElementTypes.UInt64,        ExpectResult = false},
                 new { Type = MatrixElementTypes.UInt32,        ExpectResult = false},
                 new { Type = MatrixElementTypes.UInt8,         ExpectResult = true},
                 new { Type = MatrixElementTypes.UInt16,        ExpectResult = true},
                 new { Type = MatrixElementTypes.Int8,          ExpectResult = true},
                 new { Type = MatrixElementTypes.Int16,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Int32,         ExpectResult = true},
+                new { Type = MatrixElementTypes.Int64,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Float,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Double,        ExpectResult = true}
             };
@@ -111,12 +115,14 @@ namespace DlibDotNet.Tests.Matrix
                 new { Type = MatrixElementTypes.RgbPixel,      ExpectResult = false},
                 new { Type = MatrixElementTypes.RgbAlphaPixel, ExpectResult = false},
                 new { Type = MatrixElementTypes.HsiPixel,      ExpectResult = false},
+                new { Type = MatrixElementTypes.UInt64,        ExpectResult = false},
                 new { Type = MatrixElementTypes.UInt32,        ExpectResult = false},
                 new { Type = MatrixElementTypes.UInt8,         ExpectResult = true},
                 new { Type = MatrixElementTypes.UInt16,        ExpectResult = true},
                 new { Type = MatrixElementTypes.Int8,          ExpectResult = true},
                 new { Type = MatrixElementTypes.Int16,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Int32,         ExpectResult = true},
+                new { Type = MatrixElementTypes.Int64,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Float,         ExpectResult = true},
                 new { Type = MatrixElementTypes.Double,        ExpectResult = true}
             };
@@ -156,79 +162,93 @@ namespace DlibDotNet.Tests.Matrix
             {
                 case MatrixElementTypes.UInt8:
                     if (useInc)
-                        return new MatrixRangeExp<byte>(0, 0, 0);
+                        return new MatrixRangeExp<byte>(0, 1, 0);
                     else if (useNum)
-                        return new MatrixRangeExp<byte>(0, 0, (uint)0);
+                        return new MatrixRangeExp<byte>(0, 0, (int)0);
                     else
                         return new MatrixRangeExp<byte>(0, 0);
                 case MatrixElementTypes.UInt16:
                     if (useInc)
-                        return new MatrixRangeExp<ushort>(0, 0, 0);
+                        return new MatrixRangeExp<ushort>(0, 1, 0);
                     else if (useNum)
-                        return new MatrixRangeExp<ushort>(0, 0, (uint)0);
+                        return new MatrixRangeExp<ushort>(0, 0, (int)0);
                     else
                         return new MatrixRangeExp<ushort>(0, 0);
                 case MatrixElementTypes.UInt32:
                     if (useInc)
-                        return new MatrixRangeExp<uint>(0, 0, 0);
+                        return new MatrixRangeExp<uint>(0, 1, 0);
                     else if (useNum)
                         return new MatrixRangeExp<uint>(0, 0, 0);
                     else
                         return new MatrixRangeExp<uint>(0, 0);
+                case MatrixElementTypes.UInt64:
+                    if (useInc)
+                        return new MatrixRangeExp<ulong>(0, 1, 0);
+                    else if (useNum)
+                        return new MatrixRangeExp<ulong>(0, 0, (int)0);
+                    else
+                        return new MatrixRangeExp<ulong>(0, 0);
                 case MatrixElementTypes.Int8:
                     if (useInc)
-                        return new MatrixRangeExp<sbyte>(0, 0, 0);
+                        return new MatrixRangeExp<sbyte>(0, 1, 0);
                     else if (useNum)
-                        return new MatrixRangeExp<sbyte>(0, 0, (uint)0);
+                        return new MatrixRangeExp<sbyte>(0, 0, (int)0);
                     else
                         return new MatrixRangeExp<sbyte>(0, 0);
                 case MatrixElementTypes.Int16:
                     if (useInc)
-                        return new MatrixRangeExp<short>(0, 0, 0);
+                        return new MatrixRangeExp<short>(0, 1, 0);
                     else if (useNum)
-                        return new MatrixRangeExp<short>(0, 0, (uint)0);
+                        return new MatrixRangeExp<short>(0, 0, (int)0);
                     else
                         return new MatrixRangeExp<short>(0, 0);
                 case MatrixElementTypes.Int32:
                     if (useInc)
-                        return new MatrixRangeExp<int>(0, 0, 0);
+                        return new MatrixRangeExp<int>(0, 1, 0);
                     else if (useNum)
-                        return new MatrixRangeExp<int>(0, 0, (uint)0);
+                        return new MatrixRangeExp<int>(0, 0, (int)0);
                     else
                         return new MatrixRangeExp<int>(0, 0);
+                case MatrixElementTypes.Int64:
+                    if (useInc)
+                        return new MatrixRangeExp<long>(0, 1, 0);
+                    else if (useNum)
+                        return new MatrixRangeExp<long>(0, 0, (int)0);
+                    else
+                        return new MatrixRangeExp<long>(0, 0);
                 case MatrixElementTypes.Float:
                     if (useInc)
-                        return new MatrixRangeExp<float>(0f, 0f, 0f);
+                        return new MatrixRangeExp<float>(0f, 1f, 0f);
                     else if (useNum)
-                        return new MatrixRangeExp<float>(0, 0, 0);
+                        return new MatrixRangeExp<float>(0, 0, (int)0);
                     else
                         return new MatrixRangeExp<float>(0, 0);
                 case MatrixElementTypes.Double:
                     if (useInc)
-                        return new MatrixRangeExp<double>(0d, 0d, 0d);
+                        return new MatrixRangeExp<double>(0d, 1d, 0d);
                     else if (useNum)
-                        return new MatrixRangeExp<double>(0, 0, 0);
+                        return new MatrixRangeExp<double>(0, 0, (int)0);
                     else
                         return new MatrixRangeExp<double>(0, 0);
                 case MatrixElementTypes.RgbPixel:
                     if (useInc)
                         return new MatrixRangeExp<RgbPixel>(default(RgbPixel), default(RgbPixel), default(RgbPixel));
                     else if (useNum)
-                        return new MatrixRangeExp<RgbPixel>(default(RgbPixel), default(RgbPixel), 0);
+                        return new MatrixRangeExp<RgbPixel>(default(RgbPixel), default(RgbPixel), (int)0);
                     else
                         return new MatrixRangeExp<RgbPixel>(default(RgbPixel), default(RgbPixel));
                 case MatrixElementTypes.RgbAlphaPixel:
                     if (useInc)
                         return new MatrixRangeExp<RgbAlphaPixel>(default(RgbAlphaPixel), default(RgbAlphaPixel), default(RgbAlphaPixel));
                     else if (useNum)
-                        return new MatrixRangeExp<RgbAlphaPixel>(default(RgbAlphaPixel), default(RgbAlphaPixel), 0);
+                        return new MatrixRangeExp<RgbAlphaPixel>(default(RgbAlphaPixel), default(RgbAlphaPixel), (int)0);
                     else
                         return new MatrixRangeExp<RgbAlphaPixel>(default(RgbAlphaPixel), default(RgbAlphaPixel));
                 case MatrixElementTypes.HsiPixel:
                     if (useInc)
                         return new MatrixRangeExp<HsiPixel>(default(HsiPixel), default(HsiPixel), default(HsiPixel));
                     else if (useNum)
-                        return new MatrixRangeExp<HsiPixel>(default(HsiPixel), default(HsiPixel), 0);
+                        return new MatrixRangeExp<HsiPixel>(default(HsiPixel), default(HsiPixel), (int)0);
                     else
                         return new MatrixRangeExp<HsiPixel>(default(HsiPixel), default(HsiPixel));
                 default:
