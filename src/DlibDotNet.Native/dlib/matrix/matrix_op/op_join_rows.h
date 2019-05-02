11b6dbb09a57686a45117813e9423dd0c1aa171c
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-05-02 16:00:04 +0900

    fix: nc returns nr value

--- src/DlibDotNet.Native/dlib/matrix/matrix_op/op_join_rows.h
+++ src/DlibDotNet.Native/dlib/matrix/matrix_op/op_join_rows.h
@@ -7,6 +7,7 @@
 #include <dlib/matrix.h>
 #include <dlib/image_processing/generic_image.h>
 #include <dlib/image_transforms/colormaps.h>
+#include "../../template.h"
 #include "../../shared.h"
 
 using namespace dlib;
@@ -17,250 +18,63 @@ using namespace std;
 #define ELEMENT_IN element
 #undef ELEMENT_IN
 
-#define matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns) \
-do {\
-    if (templateRows == 0 && templateColumns == 0)\
-    {\
-	    auto mat= ((matrix_op<op_join_rows<matrix<ELEMENT_IN, 0, 0>, matrix<ELEMENT_IN, 0, 0>>>*)obj);\
-        delete mat;\
-    }\
-    else if (templateRows == 0 && templateColumns == 1)\
-    {\
-	    auto mat= ((matrix_op<op_join_rows<matrix<ELEMENT_IN, 0, 1>, matrix<ELEMENT_IN, 0, 1>>>*)obj);\
-        delete mat;\
-    }\
-} while (0)
+#define matrix_op_op_join_rows_delete_template(__TYPE__, error, __ELEMENT_TYPE__, __ROWS__, __COLUMNS__, ...) \
+auto mat= ((matrix_op<op_join_rows<matrix<__TYPE__, __ROWS__, __COLUMNS__>, matrix<__TYPE__, __ROWS__, __COLUMNS__>>>*)obj);\
+delete mat;\
 
-#define matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret) \
-do {\
-    if (templateRows == 0 && templateColumns == 0)\
-    {\
-	    auto mat= ((matrix_op<op_join_rows<matrix<ELEMENT_IN, 0, 0>, matrix<ELEMENT_IN, 0, 0>>>*)obj);\
-        *ret = mat->nr();\
-    }\
-    else if (templateRows == 0 && templateColumns == 1)\
-    {\
-	    auto mat= ((matrix_op<op_join_rows<matrix<ELEMENT_IN, 0, 1>, matrix<ELEMENT_IN, 0, 1>>>*)obj);\
-        *ret = mat->nr();\
-    }\
-} while (0)
+#define matrix_op_op_join_rows_nr_template(__TYPE__, error, __ELEMENT_TYPE__, __ROWS__, __COLUMNS__, ...) \
+auto mat= ((matrix_op<op_join_rows<matrix<__TYPE__, __ROWS__, __COLUMNS__>, matrix<__TYPE__, __ROWS__, __COLUMNS__>>>*)obj);\
+*ret = mat->nr();\
 
-#define matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret) \
-do {\
-    if (templateRows == 0 && templateColumns == 0)\
-    {\
-	    auto mat= ((matrix_op<op_join_rows<matrix<ELEMENT_IN, 0, 0>, matrix<ELEMENT_IN, 0, 0>>>*)obj);\
-        *ret = mat->nc();\
-    }\
-    else if (templateRows == 0 && templateColumns == 1)\
-    {\
-	    auto mat= ((matrix_op<op_join_rows<matrix<ELEMENT_IN, 0, 1>, matrix<ELEMENT_IN, 0, 1>>>*)obj);\
-        *ret = mat->nc();\
-    }\
-} while (0)
+#define matrix_op_op_join_rows_nc_template(__TYPE__, error, __ELEMENT_TYPE__, __ROWS__, __COLUMNS__, ...) \
+auto mat= ((matrix_op<op_join_rows<matrix<__TYPE__, __ROWS__, __COLUMNS__>, matrix<__TYPE__, __ROWS__, __COLUMNS__>>>*)obj);\
+*ret = mat->nc();\
 
 #pragma endregion template
 
 DLLEXPORT void matrix_op_op_join_rows_delete(matrix_element_type type, void* obj, const int templateRows, const int templateColumns)
 {
-    switch(type)
-    {
-		case matrix_element_type::UInt8:
-            #define ELEMENT_IN uint8_t
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::UInt16:
-            #define ELEMENT_IN uint16_t
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::UInt32:
-            #define ELEMENT_IN uint32_t
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int8:
-            #define ELEMENT_IN int8_t
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int16:
-            #define ELEMENT_IN int16_t
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int32:
-            #define ELEMENT_IN int32_t
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Float:
-            #define ELEMENT_IN float
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Double:
-            #define ELEMENT_IN double
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::RgbPixel:
-            #define ELEMENT_IN rgb_pixel
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::HsiPixel:
-            #define ELEMENT_IN hsi_pixel
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::RgbAlphaPixel:
-            #define ELEMENT_IN rgb_alpha_pixel
-            matrix_op_op_join_rows_delete_template(obj, templateRows, templateColumns);
-            #undef ELEMENT_IN
-			break;
-    }
+    int error = ERR_OK;
+
+    matrix_template(type,
+                    error,
+                    matrix_template_size_template,
+                    matrix_op_op_join_rows_delete_template,
+                    templateRows,
+                    templateColumns,
+                    obj);
 }
 
 DLLEXPORT int matrix_op_op_join_rows_nc(matrix_element_type type, void* obj, const int templateRows, const int templateColumns, int* ret)
 {
-    int err = ERR_OK;
-    switch(type)
-    {
-		case matrix_element_type::UInt8:
-            #define ELEMENT_IN uint8_t
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::UInt16:
-            #define ELEMENT_IN uint16_t
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::UInt32:
-            #define ELEMENT_IN uint32_t
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int8:
-            #define ELEMENT_IN int8_t
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int16:
-            #define ELEMENT_IN int16_t
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int32:
-            #define ELEMENT_IN int32_t
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Float:
-            #define ELEMENT_IN float
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Double:
-            #define ELEMENT_IN double
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::RgbPixel:
-            #define ELEMENT_IN rgb_pixel
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::HsiPixel:
-            #define ELEMENT_IN hsi_pixel
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::RgbAlphaPixel:
-            #define ELEMENT_IN rgb_alpha_pixel
-            matrix_op_op_join_rows_nc_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-        default:
-            err = ERR_ARRAY2D_TYPE_NOT_SUPPORT;
-            break;
-    }
+    int error = ERR_OK;
 
-    return err;
+    matrix_template(type,
+                    error,
+                    matrix_template_size_template,
+                    matrix_op_op_join_rows_nc_template,
+                    templateRows,
+                    templateColumns,
+                    obj,
+                    ret);
+
+    return error;
 }
 
 DLLEXPORT int matrix_op_op_join_rows_nr(matrix_element_type type, void* obj, const int templateRows, const int templateColumns, int* ret)
 {
-    int err = ERR_OK;
-    switch(type)
-    {
-		case matrix_element_type::UInt8:
-            #define ELEMENT_IN uint8_t
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::UInt16:
-            #define ELEMENT_IN uint16_t
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::UInt32:
-            #define ELEMENT_IN uint32_t
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int8:
-            #define ELEMENT_IN int8_t
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int16:
-            #define ELEMENT_IN int16_t
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Int32:
-            #define ELEMENT_IN int32_t
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Float:
-            #define ELEMENT_IN float
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::Double:
-            #define ELEMENT_IN double
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::RgbPixel:
-            #define ELEMENT_IN rgb_pixel
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::HsiPixel:
-            #define ELEMENT_IN hsi_pixel
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-		case matrix_element_type::RgbAlphaPixel:
-            #define ELEMENT_IN rgb_alpha_pixel
-            matrix_op_op_join_rows_nr_template(obj, templateRows, templateColumns, ret);
-            #undef ELEMENT_IN
-			break;
-        default:
-            err = ERR_ARRAY2D_TYPE_NOT_SUPPORT;
-            break;
-    }
+    int error = ERR_OK;
 
-    return err;
+    matrix_template(type,
+                    error,
+                    matrix_template_size_template,
+                    matrix_op_op_join_rows_nr_template,
+                    templateRows,
+                    templateColumns,
+                    obj,
+                    ret);
+
+    return error;
 }
-
-#pragma region operator
-
-#pragma endregion oprator
 
 #endif
