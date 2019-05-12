525212eba03ee6311d4fd39384e7c7b8c7ce2d95
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-05-12 18:42:17 +0900

    fix: change file name and support cuda 10

--- nuget/docker/runBuild.sh
+++ nuget/docker/runBuild.sh
@@ -4,20 +4,38 @@ TARGET=$1
 ARCH=$2
 DDNROOT=/opt/data/DlibDotNet
 
-if [ "${TARGET}" == 'cpu' ] || [ "${TARGET}" == 'cuda' ]; then
+if [ $# -eq 3 ]; then
+   CUDA=$3
+fi
+
+if [ "${TARGET}" == 'cpu' ] ; then
 
    if [ "$2" == "32" ]; then
       OUTDIR=${DDNROOT}/nuget/${TARGET}/runtimes/linux-x86/native
+      BUILDDIR=build_linux_${TARGET}_x86
    elif [ "$2" == "64" ]; then
       OUTDIR=${DDNROOT}/nuget/${TARGET}/runtimes/linux-x64/native
+      BUILDDIR=build_linux_${TARGET}_x64
+   fi
+
+elif [ "${TARGET}" == 'cuda' ]; then
+
+   if [ "$2" == "32" ]; then
+      OUTDIR=${DDNROOT}/nuget/${TARGET}-${CUDA}/runtimes/linux-x86/native
+      BUILDDIR=build_linux_${TARGET}-${CUDA}_x86
+   elif [ "$2" == "64" ]; then
+      OUTDIR=${DDNROOT}/nuget/${TARGET}-${CUDA}/runtimes/linux-x64/native
+      BUILDDIR=build_linux_${TARGET}-${CUDA}_x64
    fi
 
 elif [ "${TARGET}" == 'arm' ]; then
 
    if [ "$2" == "32" ]; then
       OUTDIR=${DDNROOT}/nuget/${TARGET}/runtimes/linux-arm/native
+      BUILDDIR=build_linux_${TARGET}_x86
    elif [ "$2" == "64" ]; then
       OUTDIR=${DDNROOT}/nuget/${TARGET}/runtimes/linux-arm64/native
+      BUILDDIR=build_linux_${TARGET}_x64
    fi
 
 else
@@ -27,16 +45,10 @@ fi
 
 CONFIG=Release
 
-if [ "$2" == "32" ]; then
-   BUILDDIR=build_linux_${TARGET}_x86
-elif [ "$2" == "64" ]; then
-   BUILDDIR=build_linux_${TARGET}_x64
-fi
-
 cd ${DDNROOT}/src/DlibDotNet.Native
-./BuildUnix.sh ${CONFIG} ${TARGET} ${ARCH}
+./BuildUnix.sh ${CONFIG} ${TARGET} ${ARCH} ${CUDA}
 cp ${BUILDDIR}/*.so ${OUTDIR}
 
 cd ${DDNROOT}/src/DlibDotNet.Native.Dnn
-./BuildUnix.sh ${CONFIG} ${TARGET} ${ARCH}
+./BuildUnix.sh ${CONFIG} ${TARGET} ${ARCH} ${CUDA}
 cp ${BUILDDIR}/*.so ${OUTDIR}
