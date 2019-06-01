567673911cb006bac73ee43f13fb5d0d02446efc
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-06-01 13:35:35 +0900

    feat: add EmguCV examples

---
+++ examples/3rdparty/EmguCV/MatToMatrix/Program.cs
@@ -0,0 +1,33 @@
+﻿using DlibDotNet;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace MatToMatrix
+{
+
+    internal class Program
+    {
+
+        private static void Main()
+        {
+            using (var mat = CvInvoke.Imread("Lenna.png", ImreadModes.AnyColor))
+            {
+                var array = new byte[mat.Width * mat.Height * mat.ElementSize];
+                mat.CopyTo(array);
+
+                // TODO: support BGR image
+                using (var win = new ImageWindow())
+                using (var image = new DlibDotNet.Matrix<RgbPixel>(array, mat.Height, mat.Width, mat.ElementSize))
+                {
+                    // Display it all on the screen
+                    win.ClearOverlay();
+                    win.SetImage(image);
+
+                    win.WaitUntilClosed();
+                }
+            }
+        }
+
+    }
+
+}
