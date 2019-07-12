a51d685911f9c9dfb9eb8f4b067da64724f916aa
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-07-13 03:24:24 +0900

    fix: set non-root user for entrypoint

--- nuget/docker/build/ubuntu/16/runBuild.sh
+++ nuget/docker/build/ubuntu/16/runBuild.sh
@@ -10,8 +10,17 @@ fi
 
 CONFIG=Release
 
+# create non-root user
+NON_ROOT_USER=user
+USER_ID=${LOCAL_UID:-9001}
+GROUP_ID=${LOCAL_GID:-9001}
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+useradd -u $USER_ID -o -m $NON_ROOT_USER
+groupmod -g $GROUP_ID $NON_ROOT_USER
+export HOME=/home/$NON_ROOT_USER
+
 cd ${DDNROOT}/src/DlibDotNet.Native
-pwsh Build.ps1 ${CONFIG} ${TARGET} ${ARCH} ${CUDA}
+exec /usr/sbin/gosu $NON_ROOT_USER pwsh Build.ps1 ${CONFIG} ${TARGET} ${ARCH} ${CUDA}
 
 cd ${DDNROOT}/src/DlibDotNet.Native.Dnn
-pwsh Build.ps1 ${CONFIG} ${TARGET} ${ARCH} ${CUDA}
+exec /usr/sbin/gosu $NON_ROOT_USER pwsh Build.ps1 ${CONFIG} ${TARGET} ${ARCH} ${CUDA}
