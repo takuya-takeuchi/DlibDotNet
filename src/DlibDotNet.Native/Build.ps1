d6140f7464e209e7ddd271219d4d84a3405c5016
Marco Ortali <marco.ortali@live.it>
2020-08-01 07:18:01 +0200

    Added cuda 102 option in the native build scripts (#203)

--- src/DlibDotNet.Native/Build.ps1
+++ src/DlibDotNet.Native/Build.ps1
@@ -5,7 +5,7 @@
 #%3: Architecture (32/64)
 #%4: Platform (desktop,android/ios/uwp)
 #%5: Optional Argument
-#   if Target is cuda, CUDA version if Target is cuda [90/91/92/100/101]
+#   if Target is cuda, CUDA version if Target is cuda [90/91/92/100/101/102]
 #   if Target is mkl and Windows, IntelMKL directory path
 #***************************************
 Param
