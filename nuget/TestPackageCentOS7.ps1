066ea4f6dcb4dfdc61153fb0181581d9ef8f8005
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-11-22 22:56:50 +0900

    fix: wrong docker run command message

--- nuget/TestPackageCentOS7.ps1
+++ nuget/TestPackageCentOS7.ps1
@@ -77,7 +77,7 @@ foreach($BuildTarget in $BuildTargets)
       Write-Host "Docker API Version: $dockerAPIVersion" -ForegroundColor Yellow
       if ($dockerAPIVersion -ge 1.40)
       {
-         Write-Host "Start docker run --gpus all --rm -v ""$($DlibDotNetRoot):/opt/data/DlibDotNet"" -e LOCAL_UID=$(id -u $env:USER) -e LOCAL_GID=$(id -g $env:USER) -t ""$dockername"" $Version $package $OperatingSystem $OperatingSystemVersion" -ForegroundColor Green
+         Write-Host "Start docker run --gpus all --rm -v ""$($DlibDotNetRoot):/opt/data/DlibDotNet"" -e LOCAL_UID=$(id -u $env:USER) -e LOCAL_GID=$(id -g $env:USER) -t ""$dockername"" $Version $package $platformTarget $rid" -ForegroundColor Green
          docker run --gpus all --rm `
                      -v "$($DlibDotNetRoot):/opt/data/DlibDotNet" `
                      -e "LOCAL_UID=$(id -u $env:USER)" `
@@ -86,7 +86,7 @@ foreach($BuildTarget in $BuildTargets)
       }
       else
       {
-         Write-Host "Start docker run --runtime=nvidia --rm -v ""$($DlibDotNetRoot):/opt/data/DlibDotNet"" -e LOCAL_UID=$(id -u $env:USER) -e LOCAL_GID=$(id -g $env:USER) -t ""$dockername"" $Version $package $OperatingSystem $OperatingSystemVersion" -ForegroundColor Green
+         Write-Host "Start docker run --runtime=nvidia --rm -v ""$($DlibDotNetRoot):/opt/data/DlibDotNet"" -e LOCAL_UID=$(id -u $env:USER) -e LOCAL_GID=$(id -g $env:USER) -t ""$dockername"" $Version $package $platformTarget $rid" -ForegroundColor Green
          docker run --runtime=nvidia --rm `
                      -v "$($DlibDotNetRoot):/opt/data/DlibDotNet" `
                      -e "LOCAL_UID=$(id -u $env:USER)" `
@@ -96,7 +96,7 @@ foreach($BuildTarget in $BuildTargets)
    }
    else
    {
-      Write-Host "Start docker run --rm -v ""$($DlibDotNetRoot):/opt/data/DlibDotNet"" -e LOCAL_UID=$(id -u $env:USER) -e LOCAL_GID=$(id -g $env:USER) -t ""$dockername"" $Version $package $OperatingSystem $OperatingSystemVersion" -ForegroundColor Green
+      Write-Host "Start docker run --rm -v ""$($DlibDotNetRoot):/opt/data/DlibDotNet"" -e LOCAL_UID=$(id -u $env:USER) -e LOCAL_GID=$(id -g $env:USER) -t ""$dockername"" $Version $package $platformTarget $rid" -ForegroundColor Green
       docker run --rm `
                   -v "$($DlibDotNetRoot):/opt/data/DlibDotNet" `
                   -e "LOCAL_UID=$(id -u $env:USER)" `
