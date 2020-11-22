560db5cb5f4a85aa9b020b681c28f5412ff1ad6e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-11-22 01:39:16 -0800

    fix: support 111

--- nuget/TestPackage.ps1
+++ nuget/TestPackage.ps1
@@ -214,11 +214,18 @@ $tmp102.Add("$env:CUDA_PATH_V10_2\bin\curand64_10.dll")
 $tmp102.Add("$env:CUDA_PATH_V10_2\bin\cusolver64_10.dll")
 
 # For DlibDotNet.CUDA110
-$tmp102 = New-Object 'System.Collections.Generic.List[string]'
-$tmp102.Add("$env:CUDA_PATH_V10_2\bin\cublas64_11.dll")
-$tmp102.Add("$env:CUDA_PATH_V10_2\bin\cudnn64_8.dll")
-$tmp102.Add("$env:CUDA_PATH_V10_2\bin\curand64_10.dll")
-$tmp102.Add("$env:CUDA_PATH_V10_2\bin\cusolver64_10.dll")
+$tmp110 = New-Object 'System.Collections.Generic.List[string]'
+$tmp110.Add("$env:CUDA_PATH_V11_0\bin\cublas64_11.dll")
+$tmp110.Add("$env:CUDA_PATH_V11_0\bin\cudnn64_8.dll")
+$tmp110.Add("$env:CUDA_PATH_V11_0\bin\curand64_10.dll")
+$tmp110.Add("$env:CUDA_PATH_V11_0\bin\cusolver64_10.dll")
+
+# For DlibDotNet.CUDA111
+$tmp111 = New-Object 'System.Collections.Generic.List[string]'
+$tmp111.Add("$env:CUDA_PATH_V11_1\bin\cublas64_11.dll")
+$tmp111.Add("$env:CUDA_PATH_V11_1\bin\cudnn64_8.dll")
+$tmp111.Add("$env:CUDA_PATH_V11_1\bin\curand64_10.dll")
+$tmp111.Add("$env:CUDA_PATH_V11_1\bin\cusolver64_11.dll")
 
 $BuildTargets = @()
 $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x64"; Architecture = 64; Package = "DlibDotNet";         Dependencies = $null     }
@@ -230,6 +237,7 @@ $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x64"; Archite
 $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x64"; Architecture = 64; Package = "DlibDotNet.CUDA101"; Dependencies = $tmp101   }
 $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x64"; Architecture = 64; Package = "DlibDotNet.CUDA102"; Dependencies = $tmp102   }
 $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x64"; Architecture = 64; Package = "DlibDotNet.CUDA110"; Dependencies = $tmp110   }
+$BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x64"; Architecture = 64; Package = "DlibDotNet.CUDA111"; Dependencies = $tmp111   }
 $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x64"; Architecture = 64; Package = "DlibDotNet.MKL";     Dependencies = $null     }
 $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "x86"; Architecture = 32; Package = "DlibDotNet.MKL";     Dependencies = $null     }
 $BuildTargets += New-Object PSObject -Property @{PlatformTarget = "arm"; Architecture = 32; Package = "DlibDotNet.ARM";     Dependencies = $null     }
