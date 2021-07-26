f99e7b3ad4cdcdef60600b28ae4af7968b1b6543
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-07-27 00:12:50 +0900

    fix

--- examples/Xamarin/Demo/BuildIOS.ps1
+++ examples/Xamarin/Demo/BuildIOS.ps1
@@ -1,9 +1,16 @@
 $demo = $PSScriptRoot
 $root = Get-Location
+$xamarin = Split-Path $demo -Parent
+$examples = Split-Path $xamarin -Parent
+$dlibdotnet = Split-Path $examples -Parent
+$nuget = Join-Path $dlibdotnet nuget
 
 Set-Location $demo
-dotnet restore
+Set-Location Demo
+dotnet restore -s $nuget
 
+Set-Location $demo
 Set-Location Demo.iOS
+dotnet restore -s $nuget
 msbuild "/p:Configuration=Release;Platform=iPhoneSimulator" "/fileLoggerParameters:LogFile=../BuildIOS.log;Verbosity=quiet;Encoding=UTF-8"
 Set-Location $root
