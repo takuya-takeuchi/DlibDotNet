8a2586a1edc00eab03bec6e9776ce6970bc26a3b
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-07-26 23:48:23 +0900

    fix

--- src/DlibDotNet/GuiWidgets/SelectIndexedActionMediator.cs
+++ src/DlibDotNet/GuiWidgets/SelectIndexedActionMediator.cs
@@ -20,6 +20,7 @@ namespace DlibDotNet
 
         public SelectIndexedActionMediator(Action<uint> callback)
         {
+#if !DLIB_NO_GUI_SUPPORT
             if (callback == null)
                 throw new ArgumentNullException(nameof(callback));
 
@@ -28,6 +29,9 @@ namespace DlibDotNet
             var @delegate = new NativeMethods.SelectIndexedActionDelegate(this.NativeCallback);
             this._Handle = Marshal.GetFunctionPointerForDelegate(@delegate);
             this.NativePtr = NativeMethods.uint32t_action_mediator_new(this._Handle);
+#else
+            throw new NotSupportedException();
+#endif
         }
 
         #endregion
@@ -41,12 +45,16 @@ namespace DlibDotNet
         /// </summary>
         protected override void DisposeUnmanaged()
         {
+#if !DLIB_NO_GUI_SUPPORT
             base.DisposeUnmanaged();
 
             if (this.NativePtr == IntPtr.Zero)
                 return;
 
             NativeMethods.uint32t_action_mediator_delete(this.NativePtr);
+#else
+            throw new NotSupportedException();
+#endif
         }
 
         #endregion
