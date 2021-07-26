bfd92a34e9f1d63f64d8892d13a9f3c93a61465c
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-07-26 20:54:09 +0900

    fix

--- src/DlibDotNet/PInvoke/NativeMethods.cs
+++ src/DlibDotNet/PInvoke/NativeMethods.cs
@@ -19,6 +19,8 @@ namespace DlibDotNet
         /// </summary>
 #if LIB_STATIC
         public const string NativeLibrary = "__Internal";
+
+        public const string NativeDnnLibrary = "__Internal";
 #else
         public const string NativeLibrary = "DlibDotNetNative";
 
