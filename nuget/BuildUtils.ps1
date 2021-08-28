841e652684db4e6079cb7f2ab0f80ef83e1efdec
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-08-29 02:49:19 +0900

    fix

--- nuget/BuildUtils.ps1
+++ nuget/BuildUtils.ps1
@@ -1730,6 +1730,14 @@ function Build([Config]$Config)
    # current is each source directory
    $Current = Get-Location
 
+   $libName = Split-Path ${Current} -Leaf
+   $copyScript = Join-Path ${Current} "CopySource.ps1"
+   if ($libName.Contains("Lite") -and (Test-Path "${copyScript}"))
+   {
+      Write-Host "Invoke ${copyScript}" -ForegroundColor Yellow      
+      & "${copyScript}"
+   }
+
    $CMakefile = Join-Path $Current "CMakeLists.txt"
    if (!(Test-Path(${CMakefile})))
    {
@@ -1752,7 +1760,6 @@ function Build([Config]$Config)
 
    $Target = $Config.GetTarget()
    $Platform = $Config.GetPlatform()
-
 
    # revert dlib
    Reset-Dlib-Modification $Config (Join-Path $Current $Output)
