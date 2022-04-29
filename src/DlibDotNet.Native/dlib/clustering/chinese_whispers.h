c85bcefe86b331ee78f714d81960e374d3f5d4ca
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-04-29 22:44:42 +0900

    ChineseWhispers returns wrong value on Linux (#273)

--- src/DlibDotNet.Native/dlib/clustering/chinese_whispers.h
+++ src/DlibDotNet.Native/dlib/clustering/chinese_whispers.h
@@ -8,20 +8,26 @@
 using namespace dlib;
 using namespace std;
 
-DLLEXPORT unsigned long clustering_chinese_whispers(std::vector<sample_pair*>* edges,
-                                                    std::vector<unsigned long>* labels,
-                                                    const unsigned long num_iterations = 100)
+DLLEXPORT uint64_t clustering_chinese_whispers(std::vector<sample_pair*>* edges,
+                                               std::vector<uint32_t>* labels,
+                                               const uint32_t num_iterations = 100)
 {
     std::vector<sample_pair*>& e = *(static_cast<std::vector<sample_pair*>*>(edges));
     std::vector<sample_pair> edges_tmp;
-    for (int index = 0; index < e.size(); index++)
+    for (auto index = 0; index < e.size(); index++)
     {
         sample_pair& edge = *(static_cast<sample_pair*>(e[index]));
         edges_tmp.push_back(edge);
     }
 
-    std::vector<unsigned long>& l = *(static_cast<std::vector<unsigned long>*>(labels));
-    return dlib::chinese_whispers(edges_tmp, l, num_iterations);
+    // unsigned long is 4byte in windows but 8 byte in SILP64, ILP64, LP64 (UNIX/Linux, macOS/iOS)
+    std::vector<unsigned long> tmp;
+    const auto ret = dlib::chinese_whispers(edges_tmp, tmp, num_iterations);
+
+    for (auto index = 0; index < tmp.size(); index++)
+        labels->push_back(tmp[index]);
+
+    return ret;
 }
 
 #endif
